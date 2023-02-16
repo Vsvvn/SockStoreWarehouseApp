aaf7af117ec65715b6aa81581991281f33ae0bc7
Vsvvn <Vladsun@yandex.ru>
2023-02-16 11:55:45 +0300

    Первый коммит.

---
+++ src/main/java/CourseWork3stCourse/CourseWork3stCourseApplication.java
@@ -0,0 +1,12 @@
+package CourseWork3stCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork3stCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWork3stCourseApplication.class, args);
+    }
+}
---
+++ src/test/java/CourseWork3stCourse/CourseWork3stCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package CourseWork3stCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork3stCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
